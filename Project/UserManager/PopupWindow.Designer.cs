6141886a04015a6b3a285fac78065f087501f692
GerH <3111993@gmx.at>
2020-03-21 17:40:24 +0100

    Add files via upload

---
+++ Project/UserManager/PopupWindow.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace UserManager
+{
+    partial class PopupWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PopupWindow));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(290, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ClickLogin);
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(12, 12);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(374, 22);
+            this.textBoxUsername.TabIndex = 1;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(12, 40);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '•';
+            this.textBoxPassword.Size = new System.Drawing.Size(374, 22);
+            this.textBoxPassword.TabIndex = 2;
+            // 
+            // PopupWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 131);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PopupWindow";
+            this.Text = "Login to database";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+    }
+}
---
+++ Project/UserManager/PopupWindow.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserManager
+{
+    public partial class PopupWindow : Form
+    {
+        bool waterMarkActive = false;
+        public PopupWindow()
+        {
+            InitializeComponent();
+
+            //Focus on username textbox
+            this.ActiveControl = textBoxUsername;
+
+            //Add text hint
+            this.waterMarkActive = true;
+            this.textBoxUsername.ForeColor = Color.Gray;
+            this.textBoxUsername.Text = "Type username here";
+            this.textBoxUsername.GotFocus += (source, e) =>
+            {
+                if (this.waterMarkActive)
+                {
+                    this.waterMarkActive = false;
+                    this.textBoxUsername.Text = "";
+                    this.textBoxUsername.ForeColor = Color.Black;
+                }
+            };
+            this.textBoxUsername.LostFocus += (source, e) =>
+            {
+                if (!this.waterMarkActive && string.IsNullOrEmpty(this.textBoxUsername.Text))
+                {
+                    this.waterMarkActive = true;
+                    this.textBoxUsername.Text = "Type username here";
+                    this.textBoxUsername.ForeColor = Color.Gray;
+                }
+            };
+        }
+
+        private void ClickLogin(object sender, EventArgs e)
+        {   //close this form
+            this.Close();
+        }
+
+        public string EnteredText
+        {
+            get
+            {   //return Atlas Connection String
+                return ("mongodb+srv://" + textBoxUsername.Text.Replace(" ","") + ":"+ textBoxPassword.Text + "@alpencluster-acbgw.mongodb.net/test?retryWrites=true&w=majority");
+            }
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Enter)
+                this.Close();
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
